6ee8ca2c200eb91830ff6add39c44e7c1b44480f
Eddy10CM <caz_8@live.com.mx>
2016-07-19 14:24:43 -0500

    add the functions in the view show.html.twig

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -4,16 +4,28 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 
-class GenusController
+class GenusController extends Controller
 {
     /**
      * @Route("/genus/{genusName}")
      */
     public function showAction($genusName)
     {
-        return new Response('The genus: ' . $genusName);
+        $notes = [
+            'Octopus asked me a riddle, outsmarted me',
+            'I counted 8 legs... as they wrapped around me',
+            'Inked!'
+        ];
+
+        $templating = $this->container->get('templating');
+        $html = $templating->render('genus/show.html.twig',[
+            'name' => $genusName,
+            'notes' => $notes
+        ]);
+        return new Response($html);
     }
 }
